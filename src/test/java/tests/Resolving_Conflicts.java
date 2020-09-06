abb8823529502254920357be2c3bb4df9fcbe2eb
basakA5 <67756915+basakA5@users.noreply.github.com>
2020-09-05 22:58:50 -0500

    Update Resolving_Conflicts.java

--- src/test/java/tests/Resolving_Conflicts.java
+++ src/test/java/tests/Resolving_Conflicts.java
@@ -6,6 +6,7 @@ public class Resolving_Conflicts {
 
     @Test
     public static void test1(){
+        System.out.println("pull the message here from Origin master / Remote repository.");
 //        pull the message here from Origin master / Remote repository.
     }
 }
