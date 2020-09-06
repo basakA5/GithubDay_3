96fb93a3f5cfc81e00f6d8f894ce86a21937bc17
Basak Altintas <basak1altintas@gmail.com>
2020-09-05 23:03:27 -0500

    test2 is created

--- src/test/java/tests/Resolving_Conflicts.java
+++ src/test/java/tests/Resolving_Conflicts.java
@@ -7,5 +7,10 @@ public class Resolving_Conflicts {
     @Test
     public static void test1(){
 //        pull the message here from Origin master / Remote repository.
+
+    }
+    @Test
+    public static void test2(){
+        // test2 is created
     }
 }
